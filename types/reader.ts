3f9ed41c560fd7262909b2f206facbec7f62f487
AmoureuxID <134390773+AmoureuxID@users.noreply.github.com>
2026-02-27 21:21:44 +0700

    reset

--- types/reader.ts
+++
@@ -1,9 +0,0 @@
-export type ReaderChapter = {
-  title?: string
-  images?: string[]
-}
-
-export type ReaderNavigation = {
-  prev?: { slug?: string; title?: string }
-  next?: { slug?: string; title?: string }
-}
